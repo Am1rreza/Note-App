bfa8cb7e6ec86e4b24d94c753831f86f1c1ada05
Amirreza <amirreza.m13825@gmail.com>
2022-12-31 15:08:09 +0330

    createListItemHTML method added to NotesView class

--- js/NotesView.js
+++ js/NotesView.js
@@ -36,4 +36,26 @@ export default class NotesView {
       });
     });
   }
+
+  #createListItemHTML(id, title, body, updated) {
+    const MAX_BODY_LENGTH = 50;
+
+    return `
+    <div class="notes__list-item" data-node-id="${id}">
+            <div class="notes__small-title">${title}</div>
+            <div class="notes__small-body">
+            ${body.substring(0, MAX_BODY_LENGTH)}
+            ${body.length > MAX_BODY_LENGTH ? "..." : ""}
+            </div>
+            <div class="notes__small-updated">
+            ${new Date(updated).toLocaleString("en", {
+              dateStyle: "full",
+              timeStyle: "short",
+            })}
+            </div>
+          </div>
+    `;
+  }
+
+  updateNoteList(notes) {}
 }
