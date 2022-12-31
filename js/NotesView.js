ba74a0cff448147578f6d34c674f6dfa8fd00718
Amirreza <amirreza.m13825@gmail.com>
2022-12-31 14:53:49 +0330

    constructor of the NotesView class , added

--- js/NotesView.js
+++ js/NotesView.js
@@ -1,1 +1,39 @@
-export default class NotesView {}
+export default class NotesView {
+  constructor(root, handlers) {
+    this.root = root;
+
+    const { onNoteAdd, onNoteEdit } = handlers;
+    this.onNoteAdd = onNoteAdd;
+    this.onNoteEdit = onNoteEdit;
+    // add root innerHTML
+    this.root.innerHTML = `
+    <div class="notes__sidebar">
+        <div class="notes__logo">NOTE APP</div>
+        <div class="notes__list"></div>
+        <button class="notes__add">Add Note</button>
+      </div>
+      <div class="notes__preview">
+        <input type="text" class="notes__title" placeholder="Note Title" />
+        <textarea class="notes__body" placeholder="Take Some Note"></textarea>
+      </div>
+    `;
+    // get HTML tags from DOM
+    const addNoteBtn = this.root.querySelector(".notes__add");
+    const inputTitle = this.root.querySelector(".notes__title");
+    const inputBody = this.root.querySelector(".notes__body");
+    // event listeners
+    addNoteBtn.addEventListener("click", () => {
+      // run addNote method
+      this.onNoteAdd();
+    });
+
+    [inputBody, inputTitle].forEach((inputField) => {
+      inputField.addEventListener("blur", () => {
+        const newBody = inputBody.value.trim();
+        const newTitle = inputTitle.value.trim();
+
+        this.onNoteEdit(newTitle, newBody);
+      });
+    });
+  }
+}
--- js/index.js
+++ js/index.js
@@ -1,2 +1,13 @@
 import NotesAPI from "./NotesAPI.js";
 import NotesView from "./NotesView.js";
+const app = document.getElementById("app");
+
+const view = new NotesView(app, {
+  onNoteAdd() {
+    console.log("note added");
+  },
+
+  onNoteEdit(newTitle,newBody){
+    console.log(newTitle,newBody);
+  }
+});
