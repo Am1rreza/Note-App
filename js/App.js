26846a8e2bde68d0fc73808f496dd5ddd886d853
Amirreza <amirreza.m13825@gmail.com>
2023-01-01 14:27:03 +0330

    some codes in index.js moved to App.js and refactored

--- js/App.js
+++ js/App.js
@@ -1,3 +1,30 @@
+import NotesAPI from "./NotesAPI.js";
+import NotesView from "./NotesView.js";
+
 export default class App {
-  constructor() {}
+  constructor(root) {
+    this.notes = [];
+    this.activeNote = null;
+    this.view = new NotesView(root, this.#handlers);
+  }
+
+  #handlers() {
+    return {
+      onNoteAdd: () => {
+        console.log("note added");
+      },
+
+      onNoteEdit: (newTitle, newBody) => {
+        console.log(newTitle, newBody);
+      },
+
+      onNoteSelect: (noteId) => {
+        console.log(noteId);
+      },
+
+      onNoteDelete: (noteId) => {
+        console.log(noteId);
+      },
+    };
+  }
 }
--- js/index.js
+++ js/index.js
@@ -1,23 +1,4 @@
-import NotesAPI from "./NotesAPI.js";
-import NotesView from "./NotesView.js";
 import App from "./App.js";
 const root = document.getElementById("app");
-const app = new App();
+const app = new App(root);
 
-const view = new NotesView(app, {
-  onNoteAdd() {
-    console.log("note added");
-  },
-
-  onNoteEdit(newTitle, newBody) {
-    console.log(newTitle, newBody);
-  },
-
-  onNoteSelect(noteId) {
-    console.log(noteId);
-  },
-
-  onNoteDelete(noteId) {
-    console.log(noteId);
-  },
-});
